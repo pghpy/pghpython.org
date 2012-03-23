a59cda2938ae0b402e29c9fc0a0bb475af92cd2a
Jamie Forrest <jamie@answerqi.com>
2012-03-23 12:51:39 -0400

    moved db and other env-specific settings to local_settings (ignored in .gitignore), and created a local_settings.template.py for others to use

--- pghpython/settings.py
+++ pghpython/settings.py
@@ -1,24 +1,28 @@
 # Django settings for pghpython project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+# Import local settings file
+try:
+    from local_settings import *
+except ImportError:
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
+    ADMINS = (
+        # ('Your Name', 'your_email@example.com'),
+    )
 
-MANAGERS = ADMINS
+    MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': '',                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
